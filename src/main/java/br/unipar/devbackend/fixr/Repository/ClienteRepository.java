59ac0df08798dbc698bc6db8e42894ad0c8061ee
G4htk <gabriel290303@edu.unipar.br>
2026-01-07 20:02:55 -0300

    Adicionado @Repository e cliente Repository

--- src/main/java/br/unipar/devbackend/fixr/Repository/ClienteRepository.java
+++ src/main/java/br/unipar/devbackend/fixr/Repository/ClienteRepository.java
@@ -2,6 +2,8 @@ package br.unipar.devbackend.fixr.Repository;
 
 import br.unipar.devbackend.fixr.model.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 }
--- src/main/java/br/unipar/devbackend/fixr/Repository/UsuarioRepository.java
+++ src/main/java/br/unipar/devbackend/fixr/Repository/UsuarioRepository.java
@@ -1,9 +1,9 @@
 package br.unipar.devbackend.fixr.Repository;
 
-import br.unipar.devbackend.fixr.model.Prestador;
+import br.unipar.devbackend.fixr.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PrestadorRepository extends JpaRepository<Prestador, Long> {
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
